1b3b7a98dd39c6e318dcb303778dc28bfb16d080
Burrel Vann Jr <burrelvannjr@Burrels-Macbook-Pro-2019.local>
2021-01-08 10:48:06 -0800

    fixed hst group_by issue for overlaying normal curves

--- R/hst.R
+++ R/hst.R
@@ -18,6 +18,7 @@
 hst <- function(df, var1, by1, by2){
   v1 <- NULL #necessary for removing the "undefined global function" warning
   density1 <- NULL
+  group <- NULL
   bygroups <- length(match.call())-3
   n1 <- deparse(substitute(var1))
   n1 <- as.character(n1)
@@ -30,16 +31,31 @@ hst <- function(df, var1, by1, by2){
     names(df) <- c("v1","group")
     n0 <- "v1"
     n0 <- as.character(n0)
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(v1=seq(min(.x[[n0]],na.rm=T), max(.x[[n0]],na.rm=T), length=1000),
+    #                  density1=dnorm(x=v1, mean=mean(.x[[n0]],na.rm=T), sd=sd(.x[[n0]],na.rm=T))),
+    #         .id="group")
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(var1=seq(min(.x[[n1]],na.rm=T), max(.x[[n1]],na.rm=T), length=1000),
+    #                  density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+    #         .id="group")
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(v1=seq(((mean(.x[[n0]], na.rm=T))-(2.58*(((sd(.x[[n0]], na.rm=T)) )))), ((mean(.x[[n0]], na.rm=T))+(2.58*(((sd(.x[[n0]], na.rm=T)) )))), length=1000),
+    #                  density1=dnorm(x=v1, mean=mean(.x[[n0]],na.rm=T), sd=sd(.x[[n0]],na.rm=T))),
+    #         .id="group")
     dens = split(df, df$group) %>%
-      map_df(~ tibble(v1=seq(min(.x[[n0]],na.rm=T), max(.x[[n0]],na.rm=T), length=1000),
-                      density1=dnorm(x=v1, mean=mean(.x[[n0]],na.rm=T), sd=sd(.x[[n0]],na.rm=T))),
+      map_df(~ tibble(v1=seq(((mean(.x[[n0]], na.rm=T))-(2.58*(ifelse(length(.x)>1, sd(.x[[n0]], na.rm=T), 0)))), ((mean(.x[[n0]], na.rm=T))+(2.58*(ifelse(length(.x)>1, sd(.x[[n0]], na.rm=T), 0)))), length=1000),
+                      density1=dnorm(x=v1, mean=mean(.x[[n0]],na.rm=T), sd=(ifelse(length(.x)>1, sd(.x[[n0]], na.rm=T), 0)))),
              .id="group")
     b1 <- df
     b1 <- b1[,1]
     #bins <- ((2 * (IQR(b1, na.rm=T))) / (length(b1)^(1/(length(b1)))))
     bins <- diff(range(b1, na.rm=T)) / (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
     bw <- (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
-    dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    df2 <- df %>% dplyr::count(group)
+    df2$group <- as.character(df2$group)
+    dens <- dens %>% left_join(df2, by="group")
+    dens <- dens %>% mutate(density=(density1*bw*n)) #newheight is yheight * bw * length(df)
     p <- ggplot2::ggplot(data = df, aes(x=v1)) +
       geom_histogram(color="black", fill="lightgrey", binwidth = bw) +
       facet_null() +
@@ -55,15 +71,23 @@ hst <- function(df, var1, by1, by2){
     df <- df %>%
       mutate(group = "group")
     dens = split(df, df$group) %>%
-      map_df(~ tibble(var1=seq(min(.x[[n1]],na.rm=T), max(.x[[n1]],na.rm=T), length=1000),
-                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+      map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), ((mean(.x[[n1]], na.rm=T))+(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), length=1000),
+                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))),
              .id="group")
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(((sd(.x[[n1]], na.rm=T)) )))), ((mean(.x[[n1]], na.rm=T))+(2.58*(((sd(.x[[n1]], na.rm=T)) )))), length=1000),
+    #                  density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+    #         .id="group")
     b1 <- df %>% dplyr::select({{ var1 }})
     b1 <- b1[,1]
     #bins <- ((2 * (IQR(b1, na.rm=T))) / (length(b1)^(1/(length(b1)))))
     bins <- diff(range(b1, na.rm=T)) / (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
     bw <- (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
-    dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    #dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    df2 <- df %>% dplyr::count(group)
+    df2$group <- as.character(df2$group)
+    dens <- dens %>% left_join(df2, by="group")
+    dens <- dens %>% mutate(density=(density1*bw*n)) #newheight is yheight * bw * length(df)
     p <- ggplot2::ggplot(data = df, aes(x={{ var1 }})) +
       geom_histogram(color="black", fill="lightgrey", binwidth = bw) +
       facet_null() +
@@ -77,19 +101,28 @@ hst <- function(df, var1, by1, by2){
 
   if(bygroups==1) {
     df <- df %>%
-      mutate(group = {{ by1 }})
+      dplyr::mutate(group = {{ by1 }})
     title <- paste0("Histogram of '", deparse(substitute(var1)),"' by '", deparse(substitute(by1)), "'")
     #print(bygroups)
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(((sd(.x[[n1]], na.rm=T)))))), ((mean(.x[[n1]], na.rm=T))+(2.58*(((sd(.x[[n1]], na.rm=T)))))), length=1000),
+    #                  density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+    #         .id="group")
     dens = split(df, df$group) %>%
-      map_df(~ tibble(var1=seq(min(.x[[n1]],na.rm=T), max(.x[[n1]],na.rm=T), length=1000),
-                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+      map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), ((mean(.x[[n1]], na.rm=T))+(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), length=1000),
+                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))),
              .id="group")
     b1 <- df %>% dplyr::select({{ var1 }})
     b1 <- b1[,1]
     #bins <- ((2 * (IQR(b1, na.rm=T))) / (length(b1)^(1/(length(b1)))))
     bins <- diff(range(b1, na.rm=T)) / (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
     bw <- (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
-    dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    df2 <- df %>% dplyr::count(group)
+    df2$group <- as.character(df2$group)
+    #print(sapply(df2, class))
+    #print(sapply(dens, class))
+    dens <- dens %>% left_join(df2, by="group")
+    dens <- dens %>% mutate(density=(density1*bw*n)) #newheight is yheight * bw * length(df)
     p <- ggplot2::ggplot(data = df, aes(x={{ var1 }})) +
       geom_histogram(color="black", fill="lightgrey", binwidth = bw) +
       facet_wrap(~group) +
@@ -107,15 +140,23 @@ hst <- function(df, var1, by1, by2){
     title <- paste0("Histogram of '", deparse(substitute(var1)),"' by '", deparse(substitute(by1)),"' and '", deparse(substitute(by2)), "'")
     #print(bygroups)
     dens = split(df, df$group) %>%
-      map_df(~ tibble(var1=seq(min(.x[[n1]],na.rm=T), max(.x[[n1]],na.rm=T), length=1000),
-                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+      map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), ((mean(.x[[n1]], na.rm=T))+(2.58*(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))), length=1000),
+                      density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=(ifelse(length(.x)>1, sd(.x[[n1]], na.rm=T), 0)))),
              .id="group")
+    #dens = split(df, df$group) %>%
+    #  map_df(~ tibble(var1=seq(((mean(.x[[n1]], na.rm=T))-(2.58*(((sd(.x[[n1]], na.rm=T)))))), ((mean(.x[[n1]], na.rm=T))+(2.58*(((sd(.x[[n1]], na.rm=T)))))), length=1000),
+    #                  density1=dnorm(x=var1, mean=mean(.x[[n1]],na.rm=T), sd=sd(.x[[n1]],na.rm=T))),
+    #         .id="group")
     b1 <- df %>% dplyr::select({{ var1 }})
     b1 <- b1[,1]
     #bins <- ((2 * (IQR(b1, na.rm=T))) / (length(b1)^(1/(length(b1)))))
     bins <- diff(range(b1, na.rm=T)) / (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
     bw <- (2 * IQR(b1, na.rm=T) / length(b1)^(1/3))
-    dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    #dens <- dens %>% mutate(density=(density1*bw*nrow(df))) #newheight is yheight * bw * length(df)
+    df2 <- df %>% dplyr::count(group)
+    df2$group <- as.character(df2$group)
+    dens <- dens %>% left_join(df2, by="group")
+    dens <- dens %>% mutate(density=(density1*bw*n)) #newheight is yheight * bw * length(df)
     p <- ggplot2::ggplot(data = df, aes(x={{ var1 }})) +
       geom_histogram(color="black", fill="lightgrey", binwidth = bw) +
       facet_wrap(~group) +
